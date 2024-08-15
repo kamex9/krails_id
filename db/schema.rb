6800e5d90fd0289cc8c21d7976fbbd58be15455f
kamex9 <yawaraka000@gmail.com>
2024-08-15 22:59:14 +0900

    Userコントローラー作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 0) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_15_135306) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "users", force: :cascade do |t|
+    t.string "name", limit: 32, null: false
+    t.string "email", limit: 256, null: false
+    t.string "password_digest", limit: 2048, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
